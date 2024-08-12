56d3006715aff8010d69e8b146665e6bac985c3c
Mad-ArrA <56492399+Mad-ArrA@users.noreply.github.com>
2024-08-12 20:37:48 +0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -22,7 +22,7 @@ def webhook():
 
         if 'result' in data:
             event_type = data['result']
-            if event_type != null:
+            #if event_type != null:
                 lead_title = data['time']['start']['finish']
                 message = f'Новый лид создан: {lead_title}'
                 send_message_to_telegram(message)
